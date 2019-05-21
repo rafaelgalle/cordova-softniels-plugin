b2a9ea0356cbb3a21c196c08ab0a2e8e46479ef4
rafaelgalle <rafaelgalle1@gmail.com>
2019-05-21 16:07:24 -0300

    Update snBatteryOptimization.js

--- www/snBatteryOptimization.js
+++ www/snBatteryOptimization.js
@@ -1,13 +1,18 @@
 var exec = require('cordova/exec');
-var cordova = require('cordova');
 
-exports.disableBatteryOptimizations = function()
+exports.disableBatteryOptimizations = function(arg0, success, error)
 {
 	console.log('123');
-	console.log(cordova.exec(null, null, 'snBatteryOptimization', 'battery', []));
+
+	exec(success, error, "snBatteryOptimization", "battery", [arg0]);
+
+	console.log(exec(null, null, 'snBatteryOptimization', 'battery', []));
 	console.log('1234');
-    cordova.exec(null, null, 'snBatteryOptimization', 'battery', []);
+    exec(null, null, 'snBatteryOptimization', 'battery', []);
     console.log('12345');
-    console.log(cordova.exec(null, null, 'snBatteryOptimization', 'sdabattery321', []));
+    console.log(exec(null, null, 'snBatteryOptimization', 'sdabattery321', []));
     console.log('123456');
+
+
+    exec(success, error, "LogicLinkPlugin", "coolMethod", [arg0]);
 };
