200cc6a2f6756c45b54b16e35f0dda0b4e7aa975
rafaelgalle <rafaelgalle1@gmail.com>
2019-05-21 14:44:30 -0300

    1
    1

--- src/android/snBatteryOptimization.java
+++ src/android/snBatteryOptimization.java
@@ -1,4 +1,4 @@
-package cordova.softniels.plugin;
+package com.br.cordova;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
--- www/snBatteryOptimization.js
+++ www/snBatteryOptimization.js
@@ -1,8 +1,13 @@
 var exec = require('cordova/exec');
+var cordova = require('cordova');
 
 exports.disableBatteryOptimizations = function()
 {
 	console.log('123');
+	console.log(cordova.exec(null, null, 'snBatteryOptimization', 'battery', []));
+	console.log('1234');
     cordova.exec(null, null, 'snBatteryOptimization', 'battery', []);
-    console.log('1234');
+    console.log('12345');
+    console.log(cordova.exec(null, null, 'snBatteryOptimization', 'sdabattery321', []));
+    console.log('123456');
 };
