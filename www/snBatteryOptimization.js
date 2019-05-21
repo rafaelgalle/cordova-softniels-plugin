e72d7db9fd206c235800cdb5ecaa2c8ea0a34e31
rafaelgalle <rafaelgalle1@gmail.com>
2019-05-21 16:19:11 -0300

    Update snBatteryOptimization.js

--- www/snBatteryOptimization.js
+++ www/snBatteryOptimization.js
@@ -4,15 +4,16 @@ exports.disableBatteryOptimizations = function(arg0, success, error)
 {
 	console.log('123');
 
-	exec(success, error, "snBatteryOptimization", "battery", [arg0]);
-
-	console.log(exec(null, null, 'snBatteryOptimization', 'battery', []));
+	exec(success, error, "snBatteryOptimization", "battery", []);
 	console.log('1234');
-    exec(null, null, 'snBatteryOptimization', 'battery', []);
-    console.log('12345');
-    console.log(exec(null, null, 'snBatteryOptimization', 'sdabattery321', []));
-    console.log('123456');
+
+	// console.log(exec(null, null, 'snBatteryOptimization', 'battery', []));
+	// console.log('1234');
+ //    exec(null, null, 'snBatteryOptimization', 'battery', []);
+ //    console.log('12345');
+ //    console.log(exec(null, null, 'snBatteryOptimization', 'sdabattery321', []));
+ //    console.log('123456');
 
 
-    exec(success, error, "LogicLinkPlugin", "coolMethod", [arg0]);
+    
 };
