1a1cb41446b4ec7bbfb7bcef778c21a8c0a34ffe
rafaelgalle <rafaelgalle1@gmail.com>
2019-08-20 16:05:01 -0300

    Update snBatteryOptimization.java

--- src/android/snBatteryOptimization.java
+++ src/android/snBatteryOptimization.java
@@ -143,6 +143,7 @@ public class snBatteryOptimization extends CordovaPlugin {
             intent.setData(Uri.parse("package:" + pkgName));
             intent.addFlags(
                 Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP |
                 Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 // Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
                 // Intent.FLAG_ACTIVITY_REORDER_TO_FRONT |
